f033c486a8abfedcf36524b09aa6500644b52658
oleg.spakov <oleg.spakov@uta.fi>
2015-08-21 16:47:23 +0300

    angle continuum was ensured, speed range was increased

--- GazeParser.cs
+++ GazeParser.cs
@@ -17,7 +17,7 @@ namespace SmoothVolume
 
         private class Ray
         {
-            private static double AngleOffset = 0.0;
+            private static int AngleOffset = 0;
             private static double LastAngle = 0.0;
             
             public int Timestamp;
@@ -28,18 +28,7 @@ namespace SmoothVolume
             {
                 Timestamp = aTimestamp;
                 Length = aLength;
-                Angle = aAngle + AngleOffset * 2 * Math.PI;
-
-                if ((Angle - LastAngle) > Math.PI)
-                {
-                    LastAngle--;
-                    Angle = aAngle + AngleOffset * 2 * Math.PI;
-                }
-                else if ((Angle - LastAngle) < -Math.PI)
-                {
-                    LastAngle++;
-                    Angle = aAngle + AngleOffset * 2 * Math.PI;
-                }
+                Angle = EnsureAngleNotCirculed(aAngle);
             }
 
             public Ray(int aTimestamp, Point aPoint, Point aCenter)
@@ -48,12 +37,32 @@ namespace SmoothVolume
                 int dx = aPoint.X - aCenter.X;
                 int dy = aPoint.Y - aCenter.Y;
                 Length = Math.Sqrt(dx * dx + dy * dy);
-                Angle = Math.Atan2(dy, dx);
+                Angle = EnsureAngleNotCirculed(Math.Atan2(dy, dx));
             }
 
             public bool isInRange(double aMinLength, double aMaxLength)
             {
                 return aMinLength <= Length && Length <= aMaxLength;
+            }
+
+            private double EnsureAngleNotCirculed(double aAngle)
+            {
+                double angle = aAngle + AngleOffset * 2 * Math.PI;
+                if ((angle - LastAngle) > Math.PI)
+                {
+                    AngleOffset--;
+                    Console.WriteLine("-- Was {0} now {1}", angle, aAngle + AngleOffset * 2 * Math.PI);
+                    angle = aAngle + AngleOffset * 2 * Math.PI;
+                }
+                else if ((angle - LastAngle) < -Math.PI)
+                {
+                    AngleOffset++;
+                    Console.WriteLine("++ Was {0} now {1}", angle, aAngle + AngleOffset * 2 * Math.PI);
+                    angle = aAngle + AngleOffset * 2 * Math.PI;
+                }
+
+                LastAngle = angle;
+                return angle;
             }
         }
 
@@ -62,6 +71,7 @@ namespace SmoothVolume
             public double Angle { get; private set; }
             public int Duration { get; private set; }
             public double Speed { get { return Angle * 1000 / Duration; } }
+            public State State { get; set; }
             
             public GazeTrack(Ray aFirst, Ray aLast)
             {
@@ -73,12 +83,14 @@ namespace SmoothVolume
 
                 Angle = angle;
                 Duration = aLast.Timestamp - aFirst.Timestamp;
+                State = GazeParser.State.Unknown;
             }
 
             public GazeTrack(double aAngle, int aDuration)
             {
                 Angle = aAngle;
                 Duration = aDuration;
+                State = GazeParser.State.Unknown;
             }
 
             public bool isSpeedInRange(double aMin, double aMax)
@@ -90,10 +102,13 @@ namespace SmoothVolume
 
         private const int BUFFER_DURATION = 1000;   // ms
         private const int MIN_BUFFER_DURATION = (int)(0.7 * BUFFER_DURATION);   // ms
-        private const double ERROR_THRESHOLD = 0.2; // fraction
+        private const double RADIUS_ERROR_THRESHOLD = 0.2; // fraction
+        private const double SPEED_ERROR_THRESHOLD = 0.4; // fraction
         private const double ANGLE_CHANGE = 1;      // degrees
+        private const double MIN_FIX_DIST = 50;      // pixels
 
         private bool iReady = false;
+        private Point iLastPoint = Point.Empty;
         private Queue<Ray> iBuffer = new Queue<Ray>();
 
         private Point iCenter;
@@ -116,6 +131,8 @@ namespace SmoothVolume
             iCenter = new Point(aCenterX, aCenterY);
             iRadius = aRadius;
             iExpectedSpeed = aExpectedSpeed * Math.PI / 180;
+            Console.WriteLine("Radius limits: {0} - {1}", iRadius * (1.0 - RADIUS_ERROR_THRESHOLD), iRadius * (1.0 + RADIUS_ERROR_THRESHOLD));
+            Console.WriteLine("Speed limits: {0} - {1}", iExpectedSpeed * (1 - SPEED_ERROR_THRESHOLD), iExpectedSpeed * (1 + SPEED_ERROR_THRESHOLD));
         }
 
         public void reset()
@@ -127,21 +144,31 @@ namespace SmoothVolume
         public void feed(int aTimestamp, Point aPoint)
         {
             LimitBuffer(aTimestamp);
+            EnsureSmoothPursuit(aPoint);
 
             Ray newRay = new Ray(aTimestamp, aPoint, iCenter);
-            if (newRay.isInRange(iRadius * (1.0 - ERROR_THRESHOLD), iRadius * (1.0 + ERROR_THRESHOLD)))
+            if (newRay.isInRange(iRadius * (1.0 - RADIUS_ERROR_THRESHOLD), iRadius * (1.0 + RADIUS_ERROR_THRESHOLD)))
             {
                 iBuffer.Enqueue(newRay);
 
                 if (iReady)
                 {
-                    State state = ComputeState(newRay);
+                    GazeTrack track = ComputeTrack(newRay);
 
-                    if (state == State.Increase)
+                    if (track.State == State.Increase)
                         OnAngleChanged(this, new AngleChangedArgs(ANGLE_CHANGE));
-                    else if (state == State.Decrease)
+                    else if (track.State == State.Decrease)
                         OnAngleChanged(this, new AngleChangedArgs(-ANGLE_CHANGE));
+                    //Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", newRay.Length - iRadius, ToDegrees(newRay.Angle), ToDegrees(track.Angle), track.Duration, track.Speed, track.State);
                 }
+                else
+                {
+                    //Console.WriteLine("{0}", iBuffer.Count);
+                }
+            }
+            else
+            {
+                //Console.WriteLine("{0}\t{1}", newRay.Length, ToDegrees(newRay.Angle));
             }
         }
 
@@ -156,19 +183,38 @@ namespace SmoothVolume
             iReady = iReady && iBuffer.Count > 1;
         }
 
-        private State ComputeState(Ray aRayLast)
+        private void EnsureSmoothPursuit(Point aPoint)
         {
-            State result = State.Unknown;
+            if (!iLastPoint.IsEmpty)
+            {
+                int dx = aPoint.X - iLastPoint.X;
+                int dy = aPoint.Y - iLastPoint.Y;
+                double dist = Math.Sqrt(dx * dx + dy * dy);
+                if (dist > MIN_FIX_DIST)
+                {
+                    iBuffer.Clear();
+                }
+            }
 
+            iLastPoint = aPoint;
+            iReady = iReady && iBuffer.Count > 1;
+        }
+
+        private GazeTrack ComputeTrack(Ray aRayLast)
+        {
             Ray rayFirst = iBuffer.Peek();
             GazeTrack track = new GazeTrack(rayFirst, aRayLast);
-            if (track.isSpeedInRange(iExpectedSpeed * (1 - ERROR_THRESHOLD), iExpectedSpeed * (1 + ERROR_THRESHOLD)))
-                result = State.Increase;
-            else if (track.isSpeedInRange(-iExpectedSpeed * (1 + ERROR_THRESHOLD), -iExpectedSpeed * (1 - ERROR_THRESHOLD)))
-                result = State.Decrease;
+            if (track.isSpeedInRange(iExpectedSpeed * (1 - SPEED_ERROR_THRESHOLD), iExpectedSpeed * (1 + SPEED_ERROR_THRESHOLD)))
+                track.State = State.Increase;
+            else if (track.isSpeedInRange(-iExpectedSpeed * (1 + SPEED_ERROR_THRESHOLD), -iExpectedSpeed * (1 - SPEED_ERROR_THRESHOLD)))
+                track.State = State.Decrease;
 
-            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}", aRayLast.Length - iRadius, aRayLast.Angle, track.Angle, track.Duration, track.Speed, result);
-            return result;
+            return track;
+        }
+
+        private double ToDegrees(double aRadians)
+        {
+            return aRadians * 180 / Math.PI;
         }
     }
 }
